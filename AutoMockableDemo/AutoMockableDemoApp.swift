b2b85f15a1556903e44de7320377317dfc2a5bf0
Mark Turner <turner.mark@abc.net.au>
2024-08-29 15:00:13 +0930

    Add AutoMockable project.

---
+++ AutoMockableDemo/AutoMockableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoMockableDemoApp.swift
+//  AutoMockableDemo
+//
+//  Created by Mark Turner on 29/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct AutoMockableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
